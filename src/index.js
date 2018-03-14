c5d47b1a7148eafa035ae2c41ecda8dec286a79f
Jennifer Pierce <jpierce@trustedknight.com>
2018-03-14 18:06:59 -0300

    Basic functioning Auth0 https://auth0.com/docs/quickstart/spa/react/01-login

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { makeMainRoutes } from './routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routes = makeMainRoutes();
+
+ReactDOM.render(
+  routes,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ src/services/auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'closetchart.auth0.com',
+  clientId: 'mlqxtteko6fnUyACXK6U7KmZ412afU6g',
+  callbackUrl: 'http://localhost:3000/callback'
+}
