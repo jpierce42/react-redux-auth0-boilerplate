7ab50f65174ed1fcd6cb08bcf6b01b8836a06f76
Jennifer Pierce <jpierce@trustedknight.com>
2018-03-26 17:39:55 -0300

    Separate top nav component with some react-bootstrap styling

---
+++ src/layouts/TopNav.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import { Navbar, Nav, Button } from 'react-bootstrap'
+
+class TopNav extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  render() {
+    const { isAuthenticated } = this.props.auth
+
+    return (
+      <Navbar inverse fluid staticTop>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">My App</Link>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Navbar.Form pullRight>
+            {
+              !isAuthenticated() && (
+                <Button
+                  id="qsLoginBtn"
+                  bsStyle="primary"
+                  onClick={this.login.bind(this)}
+                >
+                  Log In
+                </Button>
+              )
+            }
+            {
+              isAuthenticated() && (
+                <Button
+                  id="qsLogoutBtn"
+                  bsStyle="primary"
+                  onClick={this.logout.bind(this)}
+                >
+                  Log Out
+                </Button>
+              )
+            }
+          </Navbar.Form>
+          <Navbar.Text pullRight>
+            { isAuthenticated() && (
+              'Welcome!'
+              )
+            }
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
+}
+
+TopNav.propTypes = {
+  auth: PropTypes.object
+}
+
+export default TopNav
